91de4fc2b75e97c3b53b95beb558739a1118fa2f
Kelvin Ricafort <kelvs.r@outlook.com>
2024-05-20 15:14:16 +0800

    - Project Creation - Firebase Creation - IntroActivity Creation

---
+++ app/src/main/java/com/kelvsricafort101/wordpress/onlineshop/activity/IntroActivity.kt
@@ -0,0 +1,21 @@
+package com.kelvsricafort101.wordpress.onlineshop.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kelvsricafort101.wordpress.onlineshop.MainActivity
+import com.kelvsricafort101.wordpress.onlineshop.R
+import com.kelvsricafort101.wordpress.onlineshop.databinding.ActivityIntroBinding
+
+class IntroActivity : BaseActivity() {
+    private lateinit var binding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+        }
+    }
+}
